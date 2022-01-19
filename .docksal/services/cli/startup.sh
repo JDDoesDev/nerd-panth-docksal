79ffa62cef8b340351ec6125c598f2ee9926fcf6
JD Flynn <jflynn8@gmail.com>
2022-01-19 08:50:35 -0600

    Cleaning up to use more standard things.

--- .docksal/services/cli/startup.sh
+++ .docksal/services/cli/startup.sh
@@ -3,4 +3,4 @@
 # Docker volumes are mounted as root:root by default, so we have to fix permissions on them
 echo "Changing permissions on mounted folders."
 sudo chown -R $(id -u):$(id -g) /var/www/vendor || true
-sudo chown -R $(id -u):$(id -g) /var/www/web/modules/composer || true
+sudo chown -R $(id -u):$(id -g) /var/www/web/modules || true
